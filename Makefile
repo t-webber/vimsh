7706b6770999b5ce816b3d13b470ac55ef33e8a6
Tom Webber <t.c.w.webber@gmail.com>
2025-05-05 01:02:25 +0200

    fix: stop waiting on keyboard after exit

--- Makefile
+++ Makefile
@@ -1,13 +1,18 @@
 .PHONY: all debug clean
 
-HDRS  = $(shell find src/ | grep \.h$ )
+HDRS  = $(shell find src/ | grep "\.h" )
 SRCS  = $(HDRS:.h=.c)
 SRCS += src/main.c
 
-all: debug 
+OUT = main.out
+
+all: run 
 
 debug:
-	gcc -g $(SRCS) -o main.out 
+	gcc -g $(SRCS) -o $(OUT) 
+
+run: debug
+	./$(OUT)
 
 watch:
 	watch -n 0.1 tail -n 10 b.txt
---
+++ src/shell.h
@@ -0,0 +1,16 @@
+#pragma once
+
+/// Main runner that displays the prompt, the written characters and handles the
+/// keypresses.
+void run_shell();
+
+/// Clean up before closing the shell.
+///
+/// This means disabling raw mode and going back to initial terminal mode.
+void close_shell();
+
+/// Handler for `SIGINT`, i.e., for the Ctrl+c combination key.
+void ctrl_c_handler(int x);
+
+/// Switch to raw mode to prevent defaults on keymappings and listen to them.
+void open_shell();
