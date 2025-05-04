19301e3fa55acb3036ab277807042d59fa0ccbfd
Tom Webber <t.c.w.webber@gmail.com>
2025-05-05 00:32:38 +0200

    feat: cd builtin + fix: raw mode exit

--- src/execute.c
+++ src/execute.c
@@ -1,12 +1,35 @@
 #include "execute.h"
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-void execute_command(char *const user_input)
+void cd(char *const user_input)
+{
+        bool has_path = false;
+
+        for (const char *reader = user_input + 2; *reader != '\0'; reader++)
+                if (*reader != ' ')
+                        has_path = true;
+
+        if (has_path)
+        {
+                chdir(user_input + 3);
+                return;
+        }
+
+        chdir(getenv("HOME"));
+}
+
+bool execute_command(char *const user_input)
 {
         if (strncmp("exit", user_input, 4) == 0)
-                exit(0);
+                return 1;
 
-        system(user_input);
-        return;
+        else if (strncmp("cd", user_input, 2) == 0)
+                cd(user_input);
+
+        else
+                system(user_input);
+
+        return 0;
 }
