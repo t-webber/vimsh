e824ee74c662c57fb4ddcf7b9d54c79b3db20658
Tom Webber <t.c.w.webber@gmail.com>
2025-05-05 08:28:24 +0200

    add delete_char function and add tests to Makefile

--- src/str.h
+++ src/str.h
@@ -3,5 +3,10 @@
 /// Inserts a char in a string.
 ///
 /// This performs `*insert_position = c` without deleting the data under
-/// `insert_position`. It thus shifts the whole stirng.
+/// `insert_position`. It thus shifts the whole string.
 void insert_char(char *const insert_position, char c);
+
+/// Deletes a char in a string.
+///
+/// This deletes the char `*delete_position`.
+void delete_char(char *const delete_position);
