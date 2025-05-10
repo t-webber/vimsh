eade590b87a819dfd71830951881d762d3ea4cf7
Tom Webber <t.c.w.webber@gmail.com>
2025-05-10 18:36:52 +0200

    feat(execute): implement exit builtin

--- src/execute.c
+++ src/execute.c
@@ -1,4 +1,12 @@
 #include "execute.h"
 #include <stdlib.h>
+#include <string.h>
 
-void execute_command(char *const user_input) { system(user_input); }
+void execute_command(char *const user_input)
+{
+        if (strncmp("exit", user_input, 4) == 0)
+                exit(0);
+
+        system(user_input);
+        return;
+}
